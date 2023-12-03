c55f5b37530208ddb2c86212360413b1877297ad
Alex Muller <alex@mullr.net>
2023-12-03 11:58:24 +0000

    Day 3 part 2

--- day-03/main.py
+++ day-03/main.py
@@ -1,7 +1,8 @@
 import itertools
+import uuid
 import os
 
-def coordinates_of_symbols(input):
+def coordinates_of_symbols(input, only_gears=False):
     coordinates = {}
 
     for y_value, line in enumerate(input):
@@ -9,9 +10,14 @@ def coordinates_of_symbols(input):
             if character == '.' or character.isdigit():
                 continue
             else:
+                gear_identifier = uuid.uuid4().hex
                 for x_s in (x_value-1, x_value, x_value+1):
                     for y_s in (y_value-1, y_value, y_value+1):
-                        coordinates[(x_s, y_s)] = character
+                        if only_gears:
+                            if character == '*':
+                                coordinates[(x_s, y_s)] = gear_identifier
+                        else:
+                            coordinates[(x_s, y_s)] = character
 
     return coordinates
 
@@ -49,7 +55,35 @@ def part_1(input):
                 number_is_found = True
     return total
 
+def part_2(input):
+    gears = coordinates_of_symbols(input, only_gears=True)
+    numbers = number_locations(input)
+    gears_with_numbers = {}
+    for number in numbers:
+        number_is_found = False
+        for coordinates in number['coordinates']:
+            if coordinates in gears and not number_is_found:
+                gear_id = gears[coordinates]
+                if gear_id in gears_with_numbers:
+                    gears_with_numbers[gear_id].append(number['number'])
+                else:
+                    gears_with_numbers[gear_id] = [number['number']]
+                number_is_found = True
+
+    total = 0
+    for numbers in gears_with_numbers.values():
+        if len(numbers) >= 3:
+            raise RuntimeError('Weird gear configuration')
+        if len(numbers) <= 1:
+            continue
+        else:
+            total += (numbers[0] * numbers[1])
+
+    return total
+
+
 with open(os.path.join(os.path.dirname(__file__), 'input.txt')) as file_contents:
     input = [line for line in file_contents.read().split('\n') if line]
 
 print(part_1(input))
+print(part_2(input))
