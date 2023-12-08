aedf089dc20b6a0ca788e19a9830bf80e506a253
Alex Muller <alex.muller@ft.com>
2023-12-08 22:26:48 +0000

    Day 4 part 2

--- day-04/main.py
+++ day-04/main.py
@@ -15,7 +15,8 @@ def process_card(input):
         'winners': winners,
         'numbers': numbers,
         'winner_count': winner_count,
-        'points': points
+        'points': points,
+        'count': 1
     }
 
 def part_1(input):
@@ -24,7 +25,18 @@ def part_1(input):
     total_points = sum([card['points'] for card in cards.values()])
     return total_points
 
+def part_2(input):
+    processed_cards = [process_card(day) for day in input]
+    cards = dict(zip([card['card_id'] for card in processed_cards], processed_cards))
+    for card in cards.values():
+        cards_to_add = [i+card['card_id']+1 for i in range(card['winner_count'])] * card['count']
+        for adding_card in cards_to_add:
+            cards[adding_card]['count'] += 1
+    total_cards = sum([card['count'] for card in cards.values()])
+    return total_cards
+
 with open(os.path.join(os.path.dirname(__file__), 'input.txt')) as file_contents:
     input = [line for line in file_contents.read().split('\n') if line]
 
 print(part_1(input))
+print(part_2(input))
